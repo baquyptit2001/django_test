2860f7970289311451170bc62e5be63b7dcb0592
baquyptit2001 <phamvantuantplinh@gmail.com>
2021-09-12 17:28:18 +0700

    add list

--- todolist/views.py
+++ todolist/views.py
@@ -1,21 +1,46 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import ToDoList, Item
 from .forms import CreateNewList
 
 # Create your views here.
 
+
 def index(response, id):
     list = ToDoList.objects.get(id=id)
-    item = list.item_set.all()[0]
+    print(list.item_set.all())
+    if response.method == 'POST':
+        if response.POST.get("save"):
+            for item in list.item_set.all():
+                if response.POST.get("c"+str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+        elif response.POST.get("newItem"):
+            text = response.POST.get("item")
+            if len(text) > 2:
+                list.item_set.create(text=text, complete=False)
+            else:
+                print("invalid")
+    item = list.item_set.all()
     return render(response, "todolist/list.html", {"list": list})
 
 
 def home(response):
-    return render(response, "todolist/home.html", {'name': "home"})
-    pass
+    list = ToDoList.objects.all()
+    return render(response, "todolist/home.html", {'list': list})
 
 
 def create(response):
-    form = CreateNewList()
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = ToDoList(name=n)
+            t.save()
+        return HttpResponseRedirect("/%i" % t.id)
+    else:
+        form = CreateNewList()
     return render(response, "todolist/create.html", {"form": form})
